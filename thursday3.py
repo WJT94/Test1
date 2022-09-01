94c880651bf6d9937c822d7e8cbe5e5e7ec48e15
WJT94 <williamjt94@gmail.com>
2022-09-01 23:15:36 +0100

    Modify thursday3.py

--- thursday3.py
+++ thursday3.py
@@ -9,13 +9,22 @@ books = {
     "Roald Dahl": ["The Witches", "Charlie and the Chocolate Factory"]
 }
 
-author = input("\nPlease enter an author's name: ")
-author.strip()
+while True:
+    author_input = input("\nPlease enter an author's name: ")
+    author_input = author_input.strip().lower()  # strip extra white space and convert the input to lowercase
 
-if author in books:
-    book_list = books[author].copy()  # Creates a copy of the author's book list
-    book_list.sort()  # Sorts the list in alphabetical order
-    book_string = ", ".join(book_list)  # Conjoins the items of the list into a string
-    print(f"The following books are by {author}:  {book_string}")
-else:
-    print(f"We do not have any books by {author}. Did you enter the name correctly?")
+    found_author = False
+    for author, titles in books.items():  # Iterate on each author & title pair in the dict
+        if author_input == author.strip().lower():  # if the author input is the same as the dict author
+            found_author = True  # establish that the author was found.
+            author_input = author  # edit the input to be an exact copy of how it's spelled in the dictionary
+            break
+
+    if found_author:
+        book_list = books[author_input].copy()  # Creates a copy of the author's book list
+        book_list.sort()  # Sorts the list in alphabetical order
+        book_string = ", ".join(book_list)  # Conjoins the items of the list into a string
+        print(f"The following books are by {author_input}:  {book_string}")
+        break
+    else:
+        print(f"We do not have any books by {author_input}. Did you enter the name correctly?")
